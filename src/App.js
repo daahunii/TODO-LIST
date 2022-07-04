4a700494cd1609b0925d18a921e91ae5cabcd542
daahunii <wjdekgns35@naver.com>
2022-07-04 12:47:27 +0900

    complete todo list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import React, { useState, useEffect } from "react";
 function App() {
-  return <div></div>;
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodo("");
+    setTodos((currentArray) => [todo, ...currentArray]);
+  };
+  console.log(todos);
+  return (
+    <div>
+      <h1>My TODO LIST ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholedr="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
